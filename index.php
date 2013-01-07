dadfa0d0ef46a89c3b4ac5979f6df7126c2ce97c
HolgerJeromin <mailgithub@katur.de>
2013-01-07 21:20:26 +0100

    Dokumentation Update, openpistemap, clustering

--- index.php
+++ index.php
@@ -1,13 +1,52 @@
 <?php
+//this file generates a variable $internview, which enables administration link
+//(the only php function of this js software)
 require('/var/www/sabineholgeraccess.php');
 
 /* ********************************************************************************
 	Visualisation of Geocachingpoints out of a .loc file
 	JavaScript only implementation, inspired and API conform from GMAPLOC http://www.henning-mersch.de/projects_former
-	Loc2Map Version: 2.0.0
+	Loc2Map Version: 2.0.1
 	for new version visit http://www.katur.de/
 	
-	Copyright (c) 2012, Holger Jeromin <jeromin(at)hitnet.rwth-aachen.de>
+	Uses the leaflet software with multiple plugins. 
+	js+css leaflet is used from the leaflet CDN (see http://leafletjs.com/download.html)
+	The other files are not available via CDN, you have to download the locally on your server. 
+	I have renamed a few of them, to have the directory a little bit cleaner 
+	
+	INSTALL (all urls tested with github files from january 2013):
+		js+css from https://github.com/danzel/Leaflet.markercluster/tree/master/dist
+			download leaflet.markercluster-src.js
+			download and rename MarkerCluster.Default.css to leaflet.markercluster.css
+		js+css+png from https://github.com/kartena/Leaflet.zoomslider/tree/master/src
+			download and rename L.Control.Zoomslider.js to leaflet-control-Zoomslider.js
+			download and rename L.Control.Zoomslider.ie.css to leaflet-control-Zoomslider.ie.css
+			download and rename L.Control.Zoomslider.css to leaflet-control-Zoomslider.css
+			download all images in the directory images
+		js from https://github.com/seelmann/leaflet-providers
+			download leaflet-providers-0.0.2.js
+		js for tile layer bing and google from https://github.com/shramov/leaflet-plugins/tree/master/layer/tile
+			download and rename Bing.js to leaflet-layer-bing.js
+			download and rename Google.js to leaflet-layer-google.js
+		js for permalink from https://github.com/shramov/leaflet-plugins/tree/master/control
+			download and rename Permalink.js to leaflet-control-permalink.js
+		png from http://www.katur.de/geocaching/blue.png and crshair.gif, yellow.png, red.png and green.png
+	
+	USAGE:
+		After downloading all files into one directory, you can point the browser to 
+		the corresponding URL on your server. 
+		There are a three possibilities to fill map with caches
+			- URL parameter locurl, gives red icons
+			- URL parameter greenlocurl, gives green icons
+			- fallback to hardcoded two xml files, if none locurl is found
+		various URL parameter:
+			&title=NewDocumentTitle
+			&centerWP=CacheID for centering
+			&height=height of Window
+			&width=width of Window
+			&widthLegend=width of the legend
+	
+	Copyright (c) 2013, Holger Jeromin <jeromin(at)hitnet.rwth-aachen.de>
 	
 	This software is distributed under a Creative Commons Attribution-Noncommercial 3.0 License
 	http://creativecommons.org/licenses/by-nc/3.0/de/
@@ -29,16 +68,14 @@ require('/var/www/sabineholgeraccess.php');
 		-	better error reporting
 	27-December-2012		V2.0.0
 		-	ported to leaflet with many new features
-		- Needed:
-			js+css from https://github.com/danzel/Leaflet.markercluster
-			js+css+png from https://github.com/kartena/Leaflet.zoomslider
-			js from https://github.com/seelmann/leaflet-providers
-			js for tile layer bing and google from https://github.com/shramov/leaflet-plugins
-			js for permalink from https://github.com/shramov/leaflet-plugins
+	07-January-2013		V2.0.1
+		- Dokumentation update
+		- Openpistemap overlay
+		- Clustering is optional via checkbox
 */
 
-echo '<?xml version="1.0" encoding="iso-8859-15"?>';
 ?>
+<?xml version="1.0" encoding="iso-8859-15"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
 	"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -84,7 +121,7 @@ echo '<?xml version="1.0" encoding="iso-8859-15"?>';
 					<a href="http://www.geocaching.com/profile/?guid=bb140a75-2eb5-414c-a888-8b9d2f714bbb">
 				<img style="border:0px;" alt="" src="http://img.groundspeak.com/stats/img.aspx?uid=bb140a75-2eb5-414c-a888-8b9d2f714bbb&amp;txt=Statistik+auf+Geocaching.com" /></a>
 			</div>
-			<div>
+			<div id="maplabel">
 				<img alt="" src="yellow.png"/>(<span id="CountHidden">0</span>): hidden caches; 
 				found <span id="CountFoundAll">0</span> in total: 
 				<img alt="" src="red.png"/>(<span id="CountFoundSabine">0</span>): Sabine, 
@@ -92,11 +129,12 @@ echo '<?xml version="1.0" encoding="iso-8859-15"?>';
 				<img alt="" src="green.png"/>(<span id="CountFoundBoth">0</span>): both
 				<?php if ($internview === true){ echo '(<a href="admin.php">Adminlink</a>)'; } ?>
 				<span id="idCenterMapLink"></span>
+				<input id="idClustering" type="checkbox" checked="checked"/>
+				<label for="idClustering">Clustering</label>
 			</div>
 		</div>
 	<script type="text/javascript">
 		/* <![CDATA[ */
-		
 		var xmlHttp = null;
 		// Mozilla, Opera, Chrome, Safari and Internet Explorer (from v7)
 		if (typeof XMLHttpRequest !== 'undefined') {
@@ -157,7 +195,9 @@ echo '<?xml version="1.0" encoding="iso-8859-15"?>';
 		var CacheLonMin = Number.NaN;
 		var CacheLonMax = Number.NaN;
 		
+		var markerGroup = null;
 		var markerCluster = null;
+		var markerNoCluster = null;
 		//build tbody element with table header to append menulist entrys later
 		//do not work in browser DOM for speed
 		var geocachingmenutable = document.getElementById('geocachingmenutable');
@@ -180,7 +220,9 @@ echo '<?xml version="1.0" encoding="iso-8859-15"?>';
 		function initmap() {
 			if (true) { 
 				map = L.map('geocachingmap');
-				markercluster = new L.MarkerClusterGroup({maxClusterRadius:10, disableClusteringAtZoom:11});
+				markerGroup = new L.layerGroup();
+				markerCluster = new L.MarkerClusterGroup({maxClusterRadius:10, disableClusteringAtZoom:11, zoomToBoundsOnClick: false});
+				markerNoCluster = new L.MarkerClusterGroup({disableClusteringAtZoom:1, zoomToBoundsOnClick: false});
 				
 				/******************************************************************************
 					initialize four Icons
@@ -235,8 +277,20 @@ echo '<?xml version="1.0" encoding="iso-8859-15"?>';
 				if (Parameter_Liste.locurl !== undefined){
 					//fetch xml file from locurl
 					//
-					xmlHttp.open('GET', Parameter_Liste.locurl, false);
+					var requestURL = Parameter_Liste.locurl;
+					xmlHttp.open('GET', requestURL, false);
 					xmlHttp.send(null);
+					if (xmlHttp.status !== 200){
+						var maplabel = document.getElementById("maplabel");
+						if (maplabel !== null){
+							maplabel.innerText = "could not load LOC File from: "+requestURL+" Aborting.";
+							//unhide stats bar
+							document.getElementById("stats").style.display = "inline";
+						}else{
+							alert ("could not load LOC File from: "+requestURL+" Aborting.");
+						}
+						return false;
+					}
 					var locurlXML=xmlHttp.responseXML.documentElement;
 					var locurlWaypoints = locurlXML.getElementsByTagName('waypoint');
 					//call function to insert marker to map
@@ -245,8 +299,20 @@ echo '<?xml version="1.0" encoding="iso-8859-15"?>';
 				if (Parameter_Liste.greenlocurl !== undefined){
 					//fetch xml file from greenlocurl
 					//
-					xmlHttp.open('GET', Parameter_Liste.greenlocurl, false);
+					var requestURL = Parameter_Liste.greenlocurl;
+					xmlHttp.open('GET', requestURL, false);
 					xmlHttp.send(null);
+					if (xmlHttp.status !== 200){
+						var maplabel = document.getElementById("maplabel");
+						if (maplabel !== null){
+							maplabel.innerText = "could not load LOC File from: "+requestURL+" Aborting.";
+							//unhide stats bar
+							document.getElementById("stats").style.display = "inline";
+						}else{
+							alert ("could not load LOC File from: "+requestURL+" Aborting.");
+						}
+						return false;
+					}
 					var greenlocurlXML=xmlHttp.responseXML.documentElement;
 					var greenlocurlWaypoints = greenlocurlXML.getElementsByTagName('waypoint');
 					//call function to insert marker to map
@@ -254,15 +320,40 @@ echo '<?xml version="1.0" encoding="iso-8859-15"?>';
 				}
 				
 				if (Parameter_Liste.locurl === undefined && Parameter_Liste.greenlocurl === undefined){
-					xmlHttp.open('GET', './cachedata/sabineholger-found.xml', false);
+					var requestURL = './cachedata/sabineholger-found.xml';
+					xmlHttp.open('GET', requestURL, false);
 					xmlHttp.send(null);
+					if (xmlHttp.status !== 200){
+						var maplabel = document.getElementById("maplabel");
+						if (maplabel !== null){
+							maplabel.innerText = "could not load LOC File from: "+requestURL+" Aborting.";
+							//unhide stats bar
+							document.getElementById("stats").style.display = "inline";
+						}else{
+							alert ("could not load LOC File from: "+requestURL+" Aborting.");
+						}
+						return false;
+					}
+					
 					var FoundXML=xmlHttp.responseXML.documentElement;
 					var AllWaypoints = FoundXML.getElementsByTagName('waypoint');
 					//call function to insert marker to map
 					insertWaypoints(AllWaypoints, null);
 					
-					xmlHttp.open('GET', './cachedata/sabineholger-hidden.xml', false);
+					var requestURL = './cachedata/sabineholger-hidden.xml';
+					xmlHttp.open('GET', requestURL, false);
 					xmlHttp.send(null);
+					if (xmlHttp.status !== 200){
+						var maplabel = document.getElementById("maplabel");
+						if (maplabel !== null){
+							maplabel.innerText = "could not load LOC File from: "+requestURL+" Aborting.";
+							//unhide stats bar
+							document.getElementById("stats").style.display = "inline";
+						}else{
+							alert ("could not load LOC File from: "+requestURL+" Aborting.");
+						}
+						return false;
+					}
 					var HiddenXML=xmlHttp.responseXML.documentElement;
 					var AllWaypointsHidden = HiddenXML.getElementsByTagName('waypoint');
 					//call function to insert marker to map
@@ -293,7 +384,9 @@ echo '<?xml version="1.0" encoding="iso-8859-15"?>';
 					map.fitBounds([[CacheLatMin, CacheLonMin],[CacheLatMax, CacheLonMax]]);
 				}
 				//Add clustered Markers
-				map.addLayer(markercluster);
+				markerGroup.addLayer(markerCluster);
+				markerGroup.addLayer(markerNoCluster);
+				map.addLayer(markerGroup);
 				//build a few tile layers
 				var lOpenStreetMap = L.TileLayer.provider('OpenStreetMap');
 				var lOpenStreetMapDE = L.TileLayer.provider('OpenStreetMap.DE');
@@ -303,6 +396,9 @@ echo '<?xml version="1.0" encoding="iso-8859-15"?>';
 				var lGoogleHybrid = new L.Google("HYBRID");
 				var lGoogleSat = new L.Google("SATELLITE");
 				var lBing = new L.BingLayer("Anqm0F_JjIZvT0P3abS6KONpaBaKuTnITRrnYuiJCE0WOhH6ZbE4DzeT6brvKVR5");
+				
+				var lOpenPisteMap = L.tileLayer("http://tiles.openpistemap.org/nocontours/{z}/{x}/{y}.png", {attribution:'<a href="http://openpistemap.org">OpenPisteMap</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'});
+				
 				//only add one layer to the current map
 				map.addLayer(lOpenStreetMap);
 				//add layer switcher, permalink and scale to the map
@@ -315,10 +411,20 @@ echo '<?xml version="1.0" encoding="iso-8859-15"?>';
 					'OSM.de':lOpenStreetMapDE,
 					'OpenCycleMap':lOpenCycleMap,
 					'Watercolor':lStamenWater
-					}, {"Caches":markercluster}, {collapsed:true});
+					}, {"Caches":markerGroup, "OpenPisteMap":lOpenPisteMap}, {collapsed:true});
 				map.addControl(layers);
 				map.addControl(new L.Control.Permalink({text: 'Permalink', layers: layers}));
 				map.addControl(new L.control.scale({imperial:false}));
+				
+				document.getElementById("idClustering").onchange = function(evt){
+					if(evt.target.checked === false){
+						markerCluster.clearLayers();
+						markerNoCluster.addLayers(AllCacheMarkers);
+					}else{
+						markerNoCluster.clearLayers()
+						markerCluster.addLayers(AllCacheMarkers);
+					}
+				}
 			} else {
 				document.getElementById('geocachingmap').style.backgroundColor = '#DDDDDD';
 				document.getElementById('geocachingmap').innerHTML = 'Sorry, the Map cannot be displayed.';
@@ -432,7 +538,7 @@ echo '<?xml version="1.0" encoding="iso-8859-15"?>';
 				var CacheMarker = L.marker(CachePos, {icon:CacheIcon, title:CacheID+": "+CacheName});
 				CacheMarker.bindPopup(CacheText);
 				AllCacheMarkers[CountTR] = CacheMarker;
-				markercluster.addLayer(CacheMarker);
+				markerCluster.addLayer(CacheMarker);
 				
 				/******************************************************************************
 					build menu item for marker
